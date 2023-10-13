52fab11d4d2a857f84e9778ed6de4eeb8369e8cc
sebastian <sebastian99_19@hotmail.com>
2023-10-13 12:20:09 -0500

    change port

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import express from "express"
 import managerRouter from "./routes/send-notify-sms.js"
 
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8000
 
 app.use('/api/', managerRouter)
 
 
 app.listen(PORT, async() => {
   console.log(`Listen on port ${PORT}`)
-  await fetch("http://localhost:5000/api/send-notity-movies")
+  // await fetch("http://localhost:5000/api/send-notity-movies")
 })
